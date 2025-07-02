06438ad3ff0227e8f4e4703483c711f436a81b63
bhavyaus <bhavyau@microsoft.com>
2025-07-02 09:29:41 -0700

    Show new welcome exp to existing users

--- src/vs/workbench/contrib/chat/browser/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/chatWidget.ts
@@ -736,11 +736,15 @@ export class ChatWidget extends Disposable implements IChatWidget {
 			const defaultAgent = this.chatAgentService.getDefaultAgent(this.location, this.input.currentModeKind);
 			const additionalMessage = defaultAgent?.metadata.additionalWelcomeMessage;
 
+			// TODO@havyaus remove this startup experiment once settled
 			const startupExpValue = startupExpContext.getValue(this.contextKeyService);
+			const configuration = this.configurationService.inspect('workbench.secondarySideBar.defaultVisibility');
+			const expIsActive = configuration.defaultValue !== 'hidden';
 			let welcomeContent: IChatViewWelcomeContent;
 			if (startupExpValue === StartupExperimentGroup.MaximizedChat
 				|| startupExpValue === StartupExperimentGroup.SplitEmptyEditorChat
-				|| startupExpValue === StartupExperimentGroup.SplitWelcomeChat) {
+				|| startupExpValue === StartupExperimentGroup.SplitWelcomeChat
+				|| expIsActive) {
 				welcomeContent = this.getExpWelcomeViewContent();
 				this.container.classList.add('experimental-welcome-view');
 			}
